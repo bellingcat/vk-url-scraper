cf100ee69eaa3111a91beb8f018e058c35e6d77a
msramalho <19508417+msramalho@users.noreply.github.com>
2022-06-21 12:59:45 +0200

    updated captcha logic

--- vk_url_scraper/utils.py
+++ vk_url_scraper/utils.py
@@ -35,10 +35,11 @@ def captcha_handler(captcha):
         for wait in 24 * [5]:  # tries every 5s for 2min
             print(f"sending request to {url=}", flush=True)
             r = requests.get(url)
-            print(f"got response {r.text=}", flush=True)
-            if key := regex_string.search(r.text):
-                print(f"got captcha result {key=}", flush=True)
-                return captcha.try_again(key[0])
+            if r.status_code == 200:
+                print(f"got response {r.text=}", flush=True)
+                if key := regex_string.search(r.text):
+                    print(f"got captcha result {key=}", flush=True)
+                    return captcha.try_again(key[0])
             print(f"sleeping {wait} seconds", flush=True)
             time.sleep(wait)
     else:
